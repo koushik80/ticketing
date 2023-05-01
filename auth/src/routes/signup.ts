96a9796e29d0ad63e448469cccc3d7d2bd77da98
Koushik Dey <koushik.dey@live.com>
2023-05-01 23:35:17 +0300

    subclassing custom errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -9,20 +11,18 @@ router.post('/api/users/signup', [
     .withMessage('Email must be valid'),
   body('password')
     .trim()
-    .isLength({ min: 4, max: 20})
+    .isLength({ min: 4, max: 20 })
     .withMessage('Password must be between 4 and 20 characters')
 ],
 (req: Request, res: Response) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    throw new Error('Invalid email or password');
+    throw new RequestValidationError(errors.array());
   }
 
-  //const { email, password } = req.body;
-
   console.log('Creating a user...');
-  throw new Error('Error connecting to database');
+  throw new DatabaseConnectionError();
 
   res.send({});
  }
