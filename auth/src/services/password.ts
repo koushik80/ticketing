9d218ff85629e6182c9ad63ecc36187c255d170d
Koushik Dey <koushik.dey@live.com>
2023-05-07 22:54:33 +0300

    compared hashed password

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -3,7 +3,6 @@ import { promisify } from 'util';
 
 const scryptAsync = promisify(scrypt);
 
-
 export class Password {
   static async toHash(password: string) {
     const salt = randomBytes(8).toString('hex');
@@ -12,7 +11,10 @@ export class Password {
     return `${buf.toString('hex')}.${salt}`;
   }
 
-  static compare(storedPassword: string, suppliedPassword: string) {
+  static async compare(storedPassword: string, suppliedPassword: string) {
+    const [hashedPassword, salt] = storedPassword.split('.');
+    const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
 
+    return buf.toString('hex') === hashedPassword;
   }
 }
