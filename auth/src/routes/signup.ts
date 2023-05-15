e568f60f4973e1113e2536efef4e86b7ad693275
Koushik Dey <koushik.dey@live.com>
2023-05-15 22:37:34 +0300

    generated jwt

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from 'express-validator';
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
+import jwt from 'jsonwebtoken';
 
 
 const router = express.Router();
@@ -33,6 +34,17 @@ async (req: Request, res: Response) => {
 
   const user = User.build({ email, password });
   await user.save();
+
+  // Generate JWT
+  const userJwt = jwt.sign({
+    id: user.id,
+    email: user.email
+  }, 'asdf');
+
+  // Store it on session object
+  req.session = {
+    jwt: userJwt
+  };
 
   res.status(201).send(user);
 }
