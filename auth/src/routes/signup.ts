936724c77af78598732d7075402f0743a9547728
Koushik Dey <koushik.dey@live.com>
2023-05-16 22:53:08 +0300

    accessing env variable in a pod

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -39,7 +39,9 @@ async (req: Request, res: Response) => {
   const userJwt = jwt.sign({
     id: user.id,
     email: user.email
-  }, 'asdf');
+  },
+  process.env.JWT_KEY!
+  );
 
   // Store it on session object
   req.session = {
