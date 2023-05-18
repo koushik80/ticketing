cf0bc77561e934e9c76d5f899af449f5775e059a
Koushik Dey <koushik.dey@live.com>
2023-05-18 23:28:38 +0300

    common request validation middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 
-import { RequestValidationError } from '../errors/request-validation-error';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
@@ -15,13 +15,8 @@ router.post('/api/users/signin',
       .notEmpty()
       .withMessage('You must supply a password')
   ],
-(req: Request, res: Response) => {
-  const errors = validationResult(req);
-
-  if(!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array());
-  }
- }
+  validateRequest,
+(req: Request, res: Response) => {}
 );
 
 export { router as signinRouter };
