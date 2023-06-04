76399ea5eac2ca3b82e2c05302aadf216c05db28
Koushik Dey <koushik.dey@live.com>
2023-06-05 00:17:14 +0300

    changed node env during tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -64,3 +64,15 @@ it('disallow duplicate emails', async () => {
     })
     .expect(400);
 });
+
+it('sets cookie after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'rajon@gmail.com',
+        password: '123456',
+    })
+    .expect(201);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
