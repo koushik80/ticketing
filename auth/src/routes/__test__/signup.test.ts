df71e210ad5a79bf54da3b78bd4ef56d34e9900d
Koushik Dey <koushik.dey@live.com>
2023-06-05 00:00:19 +0300

    added invalid input test

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,39 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'abcdcom',
+        password: '123456',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'abcdcom',
+        password: '1',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'rajon@gmail.com'
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: '123456'
+    })
+    .expect(400)
+});
