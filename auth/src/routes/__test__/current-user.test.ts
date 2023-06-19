7856e1072e920c2ed94634e284bb1d5b2285ea73
Koushik Dey <koushik.dey@live.com>
2023-06-19 23:34:22 +0300

    auth helper function

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,20 +2,13 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it ('responds with details about the current user', async() => {
-  const authResponse = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'rajon@gmail.com',
-      password: '123456',
-    })
-    .expect(201);
-  const cookie = authResponse.get('Set-Cookie');
+  const cookie = await global.signin();
 
   const response = await request(app)
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .send()
     .expect(200);
-  
+
   expect(response.body.currentUser.email).toEqual('rajon@gmail.com');
 });
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,11 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 beforeAll(async () => {
@@ -25,3 +30,19 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = 'rajon@gmail.com';
+  const password = '123456';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email, password
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+};
