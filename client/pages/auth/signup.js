fc1c88de382b0d0d650c093876997cdae3adf7ec
Koushik Dey <koushik.dey@live.com>
2023-06-21 23:56:57 +0300

    account signup

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,14 +1,19 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 const signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(email, password)
-  }
+    const response = await axios.post('/api/users/signup', {
+      email, password
+    });
+
+    console.log(response.data);
+  };
 
   return (
     <form onSubmit={onSubmit}>
