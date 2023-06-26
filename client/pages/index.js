a131ada6beaf27ca3fbe904fe4c37afc894b6f38
Koushik Dey <koushik.dey@live.com>
2023-06-27 01:03:02 +0300

    server side rendering exp

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import axios from 'axios';
 
-const Index = () => {
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  axios.get('/api/users/currentuser').catch((err) => {
+    console.log(err.message);
+  });
+
   return <h1>Landing Page</h1>
 };
 
-export default Index;
+LandingPage.getInitialProps = () => {
+  console.log('I am on the server!');
+
+  return {color: 'red'};
+};
+
+export default LandingPage;
