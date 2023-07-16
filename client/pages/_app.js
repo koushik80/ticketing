5428f111a9a91b631fb5b073fb6e83a31a00b5d5
Koushik Dey <koushik.dey@live.com>
2023-07-16 18:31:27 +0300

    handling multiple GetinitialProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.css';
-
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+import buildClient from '../api/build-client';
+const AppComponent = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <h1>Header!</h1>
+      <Component {...pageProps} />
+      </div>
+  );
 };
+
+AppComponent.getInitialProps = async appContext => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  console.log(pageProps);
+
+  return data;
+};
+
+export default AppComponent;
