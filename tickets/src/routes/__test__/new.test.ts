f4156b86201fe856a61631ad5d072d5dc4e1176c
Koushik Dey <koushik.dey@live.com>
2023-08-06 22:19:26 +0300

    validating title, price and test

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -21,10 +21,41 @@ it('returns a status other 401 if the user is signed in', async () => {
 });
 
 it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10,
+    })
+    .expect(400);
 
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 10,
+    })
+    .expect(400);
 });
 
 it('returns an error if invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'abkjdmnh',
+      price: -10,
+    })
+    .expect(400);
+  
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'abgmklf',
+    })
+    .expect(400);
 
 });
 
