5e0b7068ab76f09794299c1addb40176d56becfc
Koushik Dey <koushik.dey@live.com>
2023-08-09 01:01:20 +0300

    permission checked

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -9,8 +9,8 @@ it('returns a 404 if the provided id does not exist', async () => {
     .put(`/api/tickets/${id}`)
     .set('Cookie', global.signin())
     .send({
-      title: "New Title",
-      price: 20
+      title: 'New Title',
+      price: 20,
     })
     .expect(404);
 });
@@ -21,14 +21,29 @@ it('returns a 401 if the user is not authenticated', async () => {
   await request(app)
     .put(`/api/tickets/${id}`)
     .send({
-      title: "New Title",
-      price: 20
+      title: 'New Title',
+      price: 20,
     })
     .expect(401);
 });
 
 it('returns a 401 if the user does not own the ticket', async () => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'New Title',
+      price: 20,
+    })
 
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', global.signin())
+    .send({
+      title: 'random titles',
+      price: 10000,
+    })
+    .expect(401);
 });
 
 it('returns a 400 if the user provides an invalid title or price', async () => {
