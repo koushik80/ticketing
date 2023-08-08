58be5e4f842235d4992e1ed1b61fb83f478b1f99
Koushik Dey <koushik.dey@live.com>
2023-08-09 00:27:42 +0300

    updated ticket

---
+++ tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,41 @@
+import request from 'supertest';
+import { app } from '../../app';
+import mongoose from 'mongoose';
+
+it('returns a 404 if the provided id does not exist', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .set('Cookie', global.signin())
+    .send({
+      title: "New Title",
+      price: 20
+    })
+    .expect(404);
+
+});
+
+it('returns a 401 if the user is not authenticated', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .send({
+      title: "New Title",
+      price: 20
+    })
+    .expect(401);
+});
+
+it('returns a 401 if the user does not own the ticket', async () => {
+
+});
+
+it('returns a 400 if the user provides an invalid title or price', async () => {
+
+});
+
+it('updates the ticket with provided valid inputs', async () => {
+
+});
