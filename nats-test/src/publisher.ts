640c3853e784b08ff7dbf8eb1b0e025df0bca2bc
Koushik Dey <koushik.dey@live.com>
2023-09-09 19:35:59 +0300

    builded NATS test project

---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,10 @@
+import nats from 'node-nats-streaming';
+
+const stan = nats.connect('ticketing', 'abc', {
+  url: 'http://localhost:4222' // url to the NATS cluster
+});
+
+stan.on('connect', () => {
+  console.log('Publisher connected to NATS');
+});
+
