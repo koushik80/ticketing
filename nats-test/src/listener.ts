ef749780698d3b56e7fa94ca20809a7f7291c3ae
Koushik <koushik.dey@live.com>
2023-11-22 00:17:30 +0200

    manaual ack mode added

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,17 +10,22 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true);
   const subscription = stan.subscribe(
     'ticket:created',
-    'order-service-queue-group'
+    'order-service-queue-group',
+    options
   );
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
 
     if (typeof data === 'string') {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`
-      );
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
     }
+
+    msg.ack();
   });
 });
