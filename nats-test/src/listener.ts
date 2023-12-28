060567948816e9401f28354d8f7c621c308b1a0c
Koushik <koushik.dey@live.com>
2023-12-29 00:19:45 +0200

    :sparkles: Extended Listener
    Extended Listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -15,28 +15,8 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('account-service');
-
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'queue-group-name',
-    options);
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === 'string') {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
-
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
 
@@ -76,11 +56,21 @@ abstract class Listener {
       this.onMessage(parsedData, msg);
     });
   }
-
   parseMessage(msg: Message) {
     const data = msg.getData();
     return typeof data === 'string'
       ? JSON.parse(data)
       : JSON.parse(data.toString('utf8'));
+  }
+}
+
+class TicketCreatedListener extends Listener {
+  subject = 'ticket:created';
+  queueGroupName = 'payments-service';
+
+  onMessage(data: any, msg: Message) {
+    console.log('Event data!', data);
+
+    msg.ack();
   }
 }
