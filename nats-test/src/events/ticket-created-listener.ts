71a6e65914d9556604995f0dc970830a2173a000
Koushik <koushik.dey@live.com>
2023-12-30 20:51:43 +0200

    :art: Enforced Data types
    Enforced Data types

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -3,15 +3,17 @@ import { Listener } from './base-listener';
 import { TicketCreatedEvent } from './ticket-created-event';
 import { Subjects } from './subjects';
 
-export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+export class TicketCreatedListener extends
+Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log('Event data!', data);
 
-    console.log(data.name);
-    console.log(data.cost);
+    console.log(data.id);
+    console.log(data.title);
+    console.log(data.price);
 
     msg.ack();
   }
