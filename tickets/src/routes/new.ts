89e867623786904d135b1f818c9c92d938851797
Koushik <koushik.dey@live.com>
2023-12-31 19:16:16 +0200

    :sparkles: Published Ticket Creation
    Published Ticket Creation

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@finfotek/common';
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
 
 const router = express.Router();
 
@@ -22,6 +23,12 @@ router.post(
       userId: req.currentUser!.id
     });
     await ticket.save();
+    new TicketCreatedPublisher(client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId
+    });
 
     res.status(201).send(ticket);
   }
