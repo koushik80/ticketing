70c0246a05981eefa647d12775619bc065c0b368
Koushik <koushik.dey@live.com>
2024-01-01 23:00:25 +0200

    :zap: Accessing NATS client
    Accessing NATS client

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@finfotek/common';
 import { Ticket } from '../models/ticket';
 import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -23,7 +24,7 @@ router.post(
       userId: req.currentUser!.id
     });
     await ticket.save();
-    new TicketCreatedPublisher(client).publish({
+    new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
       price: ticket.price,
