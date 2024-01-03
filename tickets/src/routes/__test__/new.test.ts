6e22b61492e6f1e8dd3d3ab799c87b25f63beb68
Koushik <koushik.dey@live.com>
2024-01-04 01:02:20 +0200

    :zap: Mock Invocations
    Mock Invocations

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,8 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
-
-jest.mock('../../nats-wrapper');
+import { natsWrapper } from '../../nats-wrapper';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -80,4 +79,19 @@ it('it creates a ticket with a valid inputs', async () => {
   expect(tickets.length).toEqual(1);
   expect(tickets[0].price).toEqual(20);
   expect(tickets[0].title).toEqual(title);
+});
+
+it('publishes an event', async () => {
+  const title = 'abcdef';
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title,
+      price: 20
+    })
+    .expect(201);
+  
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
