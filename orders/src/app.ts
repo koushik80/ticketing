bb5e8fc1e1dbb606ad31415f357475957a01e448
Koushik <koushik.dey@live.com>
2024-01-07 20:35:43 +0200

    :ambulance: Skaffolded Route handlers
    Skaffolded Route handlers

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,10 +3,11 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@finfotek/common';
-import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes/index'
-import { updateTicketRouter } from './routes/update';
+
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes/index';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -19,10 +20,10 @@ app.use(
 );
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
--- orders/src/nats-wrapper.ts
+++ orders/src/nats-wrapper.ts
@@ -25,5 +25,4 @@ class NatsWrapper {
   }
   }
 
-
 export const natsWrapper = new NatsWrapper();
