ec32aa27b7e2327b2b8726dd2fa3b4409908d8d9
Koushik <koushik.dey@live.com>
2024-01-12 22:33:30 +0200

    :sparkles: Order creation and finding reserved tickets logic
    Order creation and finding reserved tickets logic

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@finfotek/common';
+import { requireAuth, validateRequest, NotFoundError, OrderStatus, BadRequestError } from '@finfotek/common';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -11,7 +13,37 @@ router.post('/api/orders', requireAuth, [
     .isEmpty()
     .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
     .withMessage('TicketId must be provided')
-], validateRequest, async (req: Request, res: Response) => {
+],
+validateRequest,
+async (req: Request, res: Response) => {
+  const { ticketId } = req.body;
+  // Find the ticket the user is trying to order in the database
+  const ticket = await Ticket.findById(ticketId);
+  if (!ticket) {
+    throw new NotFoundError();
+  }
+
+  // Making sure that the ticket is not already reserved
+  const existingOrder = await Order.findOne({
+    ticket: ticket,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete
+      ]
+    }
+  });
+  if (existingOrder) {
+    throw new BadRequestError('Ticket is already reserved');
+  }
+
+  // Calculate an expiration date for this order
+
+  // Build the order and save it to the database
+
+  // Publish an event saying that an order was created
+
   res.send({});
 });
 
